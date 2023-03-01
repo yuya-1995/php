c0327ad3a9f5c9f2351181467fb8762e5ab4cef3
yuya-1995 <sasa19951018@gmail.com>
2023-03-01 12:38:26 +0900

    php

---
+++ lesson1.php
@@ -0,0 +1,8 @@
+<?php
+//$helloWorldという変数にHello Worldという文字列を格納し出力してください。
+
+$helloWorld = 'Hello World';
+
+echo $helloWorld;
+
+?>
---
+++ lesson3.php
@@ -0,0 +1,20 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
+
+$persona = [
+        'name' => '山田',
+        'age' => 20,
+        'gender' => '女性',
+];
+
+echo $persona['name'],"<br>",$persona['age'],"歳・",$persona['gender'];
+
+?>
